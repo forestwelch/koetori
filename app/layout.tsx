9b79410f132a63046016674e563d41906568deb0
Nick Welch <nwelchr@gmail.com>
2025-10-20 07:53:40 -0700

    format

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export const metadata: Metadata = {
     icon: [
       {
         url: 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><text y="0.9em" font-size="90">🐔</text></svg>',
-        type: 'image/svg+xml',
+        type: "image/svg+xml",
       },
     ],
   },
